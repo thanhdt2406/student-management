838143c708bf245e599f1f122516962f1496bc9c
viet <vietdq2412@gmail.com>
2020-11-06 15:23:59 +0700

    add teacherService

--- src/main/java/dao/teacher_service/TeacherService.java
+++ src/main/java/dao/teacher_service/TeacherService.java
@@ -1,4 +1,28 @@
 package dao.teacher_service;
 
+import model.staff.Teacher;
+
+import java.util.List;
+
 public class TeacherService implements ITeacherService{
+
+    @Override
+    public List<Teacher> showAllteacher() {
+        return null;
+    }
+
+    @Override
+    public boolean addNewTeacher(Teacher teacher) {
+        return false;
+    }
+
+    @Override
+    public void editTeacher(int teacherID) {
+
+    }
+
+    @Override
+    public void deleteTeacher(int teacherID) {
+
+    }
 }

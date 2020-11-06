6490797cb00eda2107f37472cdda324d3813d266
thanhdt246 <dothanh24062000@gmail.com>
2020-11-06 14:47:17 +0700

    edit userDAO

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -1,5 +1,8 @@
 package controller;
 
+import dao.UserDAO;
+import model.User;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,20 +12,25 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet",urlPatterns = "/login")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        if(username.equals("admin")&&password.equals("admin")){
-            session.setAttribute("username",username);
-            session.setAttribute("password",password);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/AdminIndex.jsp");
-            dispatcher.forward(request,response);
-        } else {
-            response.sendRedirect(request.getContextPath()+"/view/login.jsp?error=0");
+        UserDAO userDAO = new UserDAO();
+        for (User user : userDAO.getAllUsers()) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                session.setAttribute("username", username);
+                session.setAttribute("password", password);
+                if(user.getRole().equals("admin")){
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("/view/AdminIndex.jsp");
+                    dispatcher.forward(request, response);
+                } else if(user.getRole().equals("student"))
+                return;
+            }
         }
+        response.sendRedirect(request.getContextPath() + "/view/login.jsp?error=0");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

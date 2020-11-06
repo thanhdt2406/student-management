d1b1df1a60193ac147718eb4f3840067280bb6f0
viet <vietdq2412@gmail.com>
2020-11-06 22:57:54 +0700

    fix conflict

--- src/main/java/model/staff/AcademicStaff.java
+++ src/main/java/model/staff/AcademicStaff.java
@@ -2,5 +2,28 @@ package model.staff;
 
 public class AcademicStaff extends Staff {
 
+    public AcademicStaff(int userId, String username, String password, String role, String signUpDate, String name) {
+        super(userId, username, password, role, signUpDate, name);
+    }
 
+    public AcademicStaff(String username, String password, String role, String signUpDate, String name) {
+        super(username, password, role, signUpDate, name);
+    }
+
+    public AcademicStaff(String username, String password, String role, String name) {
+        super(username, password, role, name);
+    }
+
+    public AcademicStaff(int userId, String username, String password, String role, String signUpDate, String name, String phoneNumber, String address, int salary) {
+        super(userId, username, password, role, signUpDate, name, phoneNumber, address, salary);
+    }
+
+    public AcademicStaff(String username, String password, String role, String signUpDate, String name, String phoneNumber, String address, int salary) {
+        super(username, password, role, signUpDate, name, phoneNumber, address, salary);
+    }
+
+    public AcademicStaff(String username, String password, String role, String name, String phoneNumber, String address, int salary) {
+        super(username, password, role, name, phoneNumber, address, salary);
+    }
 }
+
